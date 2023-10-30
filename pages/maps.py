63be693d7578f52df577f9a794f40ac6a5008bbc
emsamarante <edu.amarante18@gmail.com>
2023-10-30 14:45:17 -0300

    Criei a página map, falta acrescentar conteúdo

--- pages/maps.py
+++ pages/maps.py
@@ -1,7 +1,19 @@
-import dash
 from dash import html, dcc
+import dash_bootstrap_components as dbc
+from dash.dependencies import Output, Input, State
+import plotly.graph_objects as go
+import plotly.express as px
+import pandas as pd
 
-layout = html.Div([
-    html.H2("Aqui teremos os mapas"),
-    html.H1("Esta página tem mapas de Vancouver")
+
+df_map = pd.read_csv("data/dataset.csv", index_col=0)
+df_store_map = df_map.to_dict()
+
+layout = dbc.Container(children=[
+    # Armazenamento de dataset
+    dcc.Store(id='dataset', data=df_store_map),
+    # dcc.Store(id='dataset-fixed', data=df_store),
+    dbc.Row([
+        html.H1("Dash map")
+    ])
 ])
